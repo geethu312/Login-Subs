306e3e78a5425dc227dab33150385f63218a642f
Geethu Asokan <geethu312@gmail.com>
2024-05-30 13:19:30 +0530

    vercel changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: { port: 4200 },
+  // preview: { port: 4200 },
   server: { port: 4200 },
 });
